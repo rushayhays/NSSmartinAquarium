d956f864491e7e042de526106b0359d84c3ff5f8
rushayhays <rushayhays@outlook.com>
2022-03-01 16:06:57 -0600

    changed pixels to em and made pictures responsive

--- scripts/fish/fish.js
+++ scripts/fish/fish.js
@@ -3,12 +3,14 @@
 export const Fish = (fishObj) => {
 	return `<article class="fish-card">
 				<div><img class="fish-image" src="images/${fishObj.image}" /></div>
-				<h3 class="fish-name">${fishObj.name}</h3>
-				<ul>
-					<li class="fish-details">${fishObj.species}</li>
-					<li class="fish-details">Length: ${fishObj.length} inches</li>
-					<li class="fish-details">Found: ${fishObj.location}</li>
-					<li class="fish-details">Diet: ${fishObj.diet}</li>
-				</ul>
+				<div class="fishInfo">
+					<h3 class="fish-name">${fishObj.name}</h3>
+					<ul>
+						<li class="fish-details">${fishObj.species}</li>
+						<li class="fish-details">Length: ${fishObj.length} inches</li>
+						<li class="fish-details">Found: ${fishObj.location}</li>
+						<li class="fish-details">Diet: ${fishObj.diet}</li>
+					</ul>
+				</div>
         	</article>`
 }
